8d50b43f4e1e535653f209baf795fd55d0892409
Wolyo <alex.soriano9825@gmail.com>
2021-05-17 20:10:41 -0600

    Subida de imagenes

--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -6,6 +6,16 @@
     <x-jet-dialog-modal wire:model="open">
         <x-slot name="title">Crear nuevo post</x-slot>
         <x-slot name="content">
+
+            <div wire:loading wire:target="image" class="mb-4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                <strong class="font-bold">Imagen Cargando</strong>
+                <span class="block sm:inline">Espere un momento a que procese la imagen.</span>
+                
+              </div>
+
+            @if ($image)
+                <img class="mb-4" src="{{$image->temporaryUrl()}}">    
+            @endif            
             <div class="mb-4">
                 <x-jet-label value="Titulo del post" />
                 <x-jet-input class="w-full" type="text" wire:model="title" />
@@ -16,12 +26,16 @@
                 <textarea wire:model.defer="content" row="6" class="form-control w-full"></textarea>
                 <x-jet-input-error class="mt-2" for="content"/>
             </div>
+            <div>
+                <input type="file" wire:model="image" id="{{$identificador}}">
+                <x-jet-input-error class="mt-2" for="image"/>
+            </div>
         </x-slot>
         <x-slot name="footer">
             <x-jet-secondary-button wire:click="$set('open', false)">
                 Cancelar
             </x-jet-secondary-button>
-            <x-jet-danger-button wire:click="save" wire:loading.attr="disabled" wire:target="save" class="disabled:opacity-25">
+            <x-jet-danger-button wire:click="save" wire:loading.attr="disabled" wire:target="save, image" class="disabled:opacity-25">
                 Crear post
             </x-jet-danger-button>
 
