e92aa66a1136c31505c8bdac1272fd631901587c
Wolyo <alex.soriano9825@gmail.com>
2021-05-18 16:58:31 -0600

    Componentes de anidamiento, editar)

--- resources/views/livewire/edit-post.blade.php
+++ resources/views/livewire/edit-post.blade.php
@@ -1,12 +1,14 @@
 <div>
-    <x-jet-danger-button wire:click="$set('open', true)">
-        Crear nuevo post
-    </x-jet-danger-button>
+    <a class="btn btn-green" wire:click="$set('open', true)">
+        <i class="fas fa-edit"></i>
+    </a>
 
     <x-jet-dialog-modal wire:model="open">
-        <x-slot name="title">Crear nuevo post</x-slot>
-        <x-slot name="content">
+        <x-slot name='title'>
+            Editar el post
+        </x-slot>
 
+        <x-slot name="content">
             <div wire:loading wire:target="image"
                 class="mb-4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
                 <strong class="font-bold">Imagen Cargando</strong>
@@ -16,32 +18,30 @@
 
             @if ($image)
                 <img class="mb-4" src="{{ $image->temporaryUrl() }}">
+            @else
+                <img src="{{Storage::url($post->image)}}">
             @endif
             <div class="mb-4">
                 <x-jet-label value="Titulo del post" />
-                <x-jet-input class="w-full" type="text" wire:model="title" />
-                <x-jet-input-error class="mt-2" for="title" />
+                <x-jet-input wire:model="post.title" type="text" class="w-full" />
             </div>
             <div class="mb-4">
                 <x-jet-label value="Contenido del post" />
-                <textarea wire:model.defer="content" row="6" class="form-control w-full"></textarea>
-                <x-jet-input-error class="mt-2" for="content" />
+                <textarea wire:model="post.content" rows="6" class="form-control w-full"></textarea>
             </div>
             <div>
                 <input type="file" wire:model="image" id="{{ $identificador }}">
                 <x-jet-input-error class="mt-2" for="image" />
             </div>
         </x-slot>
+
         <x-slot name="footer">
             <x-jet-secondary-button wire:click="$set('open', false)">
                 Cancelar
             </x-jet-secondary-button>
-            <x-jet-danger-button wire:click="save" wire:loading.attr="disabled" wire:target="save, image"
-                class="disabled:opacity-25">
-                Crear post
+            <x-jet-danger-button wire:click="save" wire:loading.attr="disabled" class="disabled:opacity-25">
+                Actualizar
             </x-jet-danger-button>
-
-
         </x-slot>
     </x-jet-dialog-modal>
 </div>
